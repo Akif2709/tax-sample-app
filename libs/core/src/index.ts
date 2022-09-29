99c669473b03320435f8e6121bc6e82bbf9b08da
akif <akif@backbase.com>
2022-09-29 23:42:08 +0200

    add firebase

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/core.module';
 export { EmptyPageComponent } from './lib/components/empty-page/empty-page.component';
 export { AdviesSignalenService } from './lib/services/advies-signalen.service';
 export * from './lib/models';
+export  {NavbarComponent} from './lib/components/navbar/navbar.component'
